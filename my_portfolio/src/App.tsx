178f561251243ee167d12c225471aff7ba9bb156
seasmoker <2k4samya@gmail.com>
2025-02-27 00:25:20 +0530

    React@Vite+Tailwind.css

---
+++ my_portfolio/src/App.tsx
@@ -0,0 +1,13 @@
+import Header from './Components/Header.tsx'
+import './App.css'
+
+function App() {
+  return(
+    
+    <Header></Header>
+
+  );
+  
+}
+
+export default App
---
+++ my_portfolio/src/Components/Header.tsx
@@ -0,0 +1,19 @@
+
+
+function Header() {
+    return(
+        <div className="font-sans text-amber-50 flex justify-between max-w-[1700px] items-center px-10 py-15 m-auto ">
+            <h1 className="w-full text-3xl font-bold  ">seasmoker.</h1>
+            <ul className="flex">
+                <li className="p-4 font-medium">Home</li>
+                <li className="p-4 font-medium">Home</li>
+                <li className="p-4 font-medium">Home</li>
+                <li className="p-4 font-medium">Home</li>
+            </ul>
+
+        </div>
+        
+    );
+}
+
+export default Header
