765f8e7de6bdccdda22793fba25d2467fb50dd7d
seasmoker <2k4samya@gmail.com>
2025-03-04 14:22:42 +0530

    minor changes

--- my_portfolio/src/Components/Header.tsx
+++ my_portfolio/src/Components/Header.tsx
@@ -2,15 +2,8 @@
 
 function Header() {
     return(
-        <div className="font-sans text-amber-50 flex justify-between max-w-[1700px] items-center px-10 py-15 m-auto ">
-            <h1 className="w-full text-3xl font-bold tracking-widest "><a href="">samya.</a></h1>
-            <ul className="flex ">
-                <li className="p-7 font-medium tracking-wider"><a href="">Home</a></li>
-                <li className="p-7 font-medium tracking-wider"><a href="">About</a></li>
-                <li className="p-7 font-medium tracking-wider"><a href="">Projects</a></li>
-                <li className="p-7 font-medium tracking-wider"><a href="">Contact</a></li>
-            </ul>
-
+        <div className="text-amber-50 flex justify-center m-auto py-25 ">
+            <h1 className="max-w-full text-3xl font-light tracking-[7px] text-center">samya.</h1>
         </div>
         
     );
--- my_portfolio/src/Components/Hero.tsx
+++ my_portfolio/src/Components/Hero.tsx
@@ -1,14 +1,12 @@
 
 function Hero() {
     return(
-        <div className="text-amber-50 max-w-[1700px] flex items-center justify-center py-50">
-            <h1 className="max-w-full text-5xl font-black text-center">the_seasmoker</h1>
+        <div className="text-amber-50 max-w-[1700px] flex justify-center m-auto py-50">
+            <h1 className="max-w-full text-5xl font-light tracking-widest text-center ">the_seasmoker</h1>
         </div>
         
 
     );
-
-
 
 
 
